82ae6d4dffe1d94b4904864f3a6a6730ca1d6462
Fuzail Ahmed Siddiqui <fuzailsiddiqui499@gmail.com>
2025-01-18 14:31:25 +0530

    17-1-25

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <div className='max-2xl:font-sans h-screen max-w-5xl ml-auto mr-auto pl-5 pr-5
+      max-xl:
+      max-lg:
+      max-md:'>
+      <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default App
